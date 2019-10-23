93c661017aeff10f6dae80844f0f1eadce8781ad
Ricardocampos <ricardocampox@hotmail.com>
2019-10-23 20:29:37 +0100

    criação das classes

--- src/main/java/BookRunner.java
+++ src/main/java/BookRunner.java
@@ -1,7 +1,19 @@
+import models.Book;
+
 public class BookRunner {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        Book book1 = new Book();
+        book1.setTitle("The art Of Computer Programing");
+
+        Book book2 = new Book();
+        book2.setTitle("Effective Java");
+
+
+
+        System.out.println("O nome do livro e " + book1.getTitle());
+        System.out.println("O nome do livro e " + book2.getTitle());
     }
+
 }
--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,5 +1,17 @@
 package models;
 
 public class Book {
+private String title;
+
+    public String getTitle()
+
+    {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 
 }
